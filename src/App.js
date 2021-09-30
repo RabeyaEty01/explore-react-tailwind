88581185b028e3f598780f29acf0c3909471aad6
RabeyaEty01 <etyrabeya01@gmail.com>
2021-09-30 20:18:09 +0600

    react ttailwind explored

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="bg-yellow-200 text-4xl">This is my Text</p>
+      <p className="text-primary">Hello my text</p>
+      <p className="text-custom">This is my own tailwind class</p>
+
+      <form>
+        <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent ..." />
+        <button class="bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ...">
+          Sign up
+        </button>
+        <br />
+        <button class="bg-red-500 hover:bg-red-700 ...">
+          Hover me
+        </button>
+      </form>
     </div>
   );
 }
